91a7a99198857f736c6baa84e6e7c5f70125fbc3
Christine Le <christinele@csu.fullerton.edu>
2019-09-10 16:25:23 -0700

    added retrieve server data functionality

--- components/student.js
+++ components/student.js
@@ -44,7 +44,13 @@ class Student {
 	ESTIMATED TIME: 30 minutes
 	*/
 	getData() {
-
+		return this.data;
+		// return this.data ={
+		// 	id: this.id,
+		// 	name: this.name,
+		// 	course: this.course,
+		// 	grade: this.grade
+		// };
 	}
 
 	/* render - create and return a table row (TR) with 4 table cells (TD) in them:
@@ -65,7 +71,23 @@ class Student {
 	ESTIMATED TIME: 2 hours
 	*/
 	render() {
+		this.domElements.row = $('<tr>')
+		//create the elements to append to tr
+		this.domElements.name = $('<td>').addClass('col-xs-4.col-md-4').text(this.data.name);
+		this.domElements.course = $('<td>').addClass('col-xs-4.col-md-4').text(this.data.course);
+		this.domElements.grade = $('<td>').addClass('col-xs-4.col-md-4').text(this.data.grade);
+		this.domElements.operations = $('<td>').addClass('col-xs-4.col-md-4')
+		//create another element button with an event handler to handle clicks and use a callback function. Then append the button to the operations.
+		this.domElements.deleteButton = $('<button>').on('click', this.handleDelete).text('Delete');
+		this.domElements.operations.append(this.domElements.deleteButton);
 
+		//append each td into the tr
+		this.domElements.row.append(this.domElements.name);
+		this.domElements.row.append(this.domElements.course);
+		this.domElements.row.append(this.domElements.grade);
+		this.domElements.row.append(this.domElements.operations)
+
+		return this.domElements.row;
 	}
 
 	/* handleDelete - call the SGT_template delete callback, and remove this student's dom element
@@ -76,7 +98,8 @@ class Student {
 	ESTIMATED TIME: 15 minutes
 	*/
 	handleDelete() {
-
+		this.deleteCallback(this.data.id);
+		this.domElements.row.remove();
 	}
 
 	/* update - change a value in the student record
@@ -95,7 +118,16 @@ class Student {
 	return: (boolean) true if it was changed, false if it was not
 	ESTIMATED TIME: 1.5 hours
 	*/
-	update() {
-
+	update(updateField, updateValue) {
+		if (updateField === 'id' || updateField === 'name' || updateField === 'course' || updateField === 'grade') {
+			if (typeof this.data[updateField] === 'number') {
+				this.data[updateField] = parseInt(updateValue);
+			} else {
+				this.data[updateField] = updateValue;
+			}
+			this.domElements[updateField].text(updateValue);
+			return true;
+		}
+		return false;
 	}
 }
