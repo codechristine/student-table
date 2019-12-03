b6cae735931f2ae1d3251e8a3e91ac17ac40503d
codechristine <christinele.dev@gmail.com>
2019-12-02 16:03:52 -0800

    updated file paths and deleted extraneous folders for final deployment

--- database/student-table.sql
+++ database/student-table.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 16, 2019 at 11:50 AM
+-- Generation Time: Dec 02, 2019 at 03:45 PM
 -- Server version: 5.7.27-0ubuntu0.18.04.1
 -- PHP Version: 7.2.24-0ubuntu0.18.04.1
 
@@ -23,9 +23,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+CREATE TABLE `courses` (
+  `course` varchar(50) NOT NULL,
+  `professor` varchar(100) NOT NULL,
+  `id` mediumint(8) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`course`, `professor`, `id`) VALUES
+('art', 'Pollock', 1),
+('computer science', 'Einstein', 2),
+('history', 'Smith', 3),
+('geometry', 'Chi', 4);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `grades`
 --
 
+DROP TABLE IF EXISTS `grades`;
 CREATE TABLE `grades` (
   `name` varchar(100) NOT NULL,
   `course` varchar(50) NOT NULL,
@@ -38,14 +62,19 @@ CREATE TABLE `grades` (
 --
 
 INSERT INTO `grades` (`name`, `course`, `grade`, `id`) VALUES
-('John Doe', 'art', 90, 1),
-('Lucy Lu', 'computer science', 80, 2),
-('Benjamin Button', 'history', 75, 3),
-('Marley Mo', '', 85, 4);
+('Benjamin Button', 'history', 100, 2),
+('Jane Doe', 'music', 50, 5),
+('Marley Moe', 'art', 90, 9);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `grades`
@@ -58,10 +87,15 @@ ALTER TABLE `grades`
 --
 
 --
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT for table `grades`
 --
 ALTER TABLE `grades`
-  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
